bfb2b81a6c79163109c45598d967ecafe998bb3f
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-28 16:46:15 -0400

    factored state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import UserCard from "./components/UserCard";
-import UserCard2 from "./components/UserCard2";
-import UserCard3 from "./components/UserCard3";
 import "./App.css";
 
 function App() {
   return (
     <div className="App"> 
+      <div className='Card'>
+       <UserCard/> 
+      </div>
+      <div className='Card'>
+       <UserCard/> 
+      </div>
       <div className='Card'>
        <UserCard/> 
       </div>
